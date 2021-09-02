279286b012d6281646f5f6d7f549ada329daba21
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-02 17:27:58 +0600

    Make the server build client on every change

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     },  
     plugins: [
         new HTMLWebpackPlugin({
-        template: "index.html"
-        }),
+            template: "src/client/entry/index.html"
+        })
     ]
 }
