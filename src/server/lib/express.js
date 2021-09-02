ed4d9c63088de25c47ad5786e0af5ffbc4953082
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-02 12:16:44 +0600

    Setup react with express server

--- src/server/lib/express.js
+++ src/server/lib/express.js
@@ -5,11 +5,24 @@ const cookieParser = require('cookie-parser')
 const config = require(path.join(process.cwd(), 'src/server/config/config'));
 require(path.join(process.cwd(), 'src/server/strategies'));
 
+const DIST_DIR = path.join(__dirname, "dist");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("public"));
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+    res.sendFile(HTML_FILE, function(err){
+       if(err){
+          res.status(500).send(err);
+       }
+    });
+});
 
 app.listen(config.PORT, () => {
   console.info(`Server listening on ${config.PORT}`);
