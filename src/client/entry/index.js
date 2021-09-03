07e5080554a3ef36acb2d4250ce1ec3d7bfef66e
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-03 09:51:59 +0600

    Setup bootstrap

--- src/client/entry/index.js
+++ src/client/entry/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import App from "./app.component";
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import "./styles.scss";
+
 
 const appRouting = (
   <Router>
