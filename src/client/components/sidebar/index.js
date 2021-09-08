f9b13406734f3a3c1a13d378c9cc5370a1ad450d
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-08 20:59:59 +0600

    Style sidebar

--- src/client/components/sidebar/index.js
+++ src/client/components/sidebar/index.js
@@ -1,28 +1,31 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './style.css';
 function SidebarItem({ path, name }) {
-    return <li class="nav-item">
-        <Link to={path} class="nav-link active" aria-current="page">
-            {name}
+    return <div className="menuitem">
+        <Link to={path} aria-current="page">
+            <span>{name}</span>
         </Link>
-    </li>
+    </div>
 }
 
 function Sidebar ({ menuItems }) {
     return (
         <div
-            class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark"
-            style={{ "width": "280px", "height": "100%" }}
-        >   
-            <ul class="nav nav-pills flex-column mb-auto">
-                {
-                    menuItems && 
-                    menuItems.length && 
-                    menuItems.map(i => <SidebarItem path={i.path} name={i.name} />)
-                }
-            </ul>
+            className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sidebar"
+        >
+            <div className="nav nav-pills flex-column mb-auto expandable_menu_section">
+                <label className="expandable_menu_item" for="expandable_menuitem">Projects</label>
+                <input class="hidden" id="expandable_menuitem" type="checkbox" />
+                <div className="expanded_section">
+                    {menuItems &&
+                        menuItems.length &&
+                        menuItems.map((i) => (
+                            <SidebarItem path={i.path} name={i.name} />
+                        ))}
+                </div>
+            </div>
         </div>
     );
 }
