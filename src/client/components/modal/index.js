302c2cd0b75bfc08e27277d4af5d11114ad553e6
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-11 22:32:46 +0600

    Change the default style of the modal

--- src/client/components/modal/index.js
+++ src/client/components/modal/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactModal from 'react-modal';
 
 function Modal({ children, ...rest }) {
-    return <ReactModal {...rest} ariaHideApp={false} >{children}</ReactModal>
+    return <ReactModal 
+        ariaHideApp={false} 
+        style={{ 
+            overlay: { backgroundColor: 'rgba(255, 255, 255, 0)' },
+            content: { 
+                backgroundColor: '#202020', 
+                boxShadow: '0px 0px 20px #131313', 
+                border: 0,
+                inset: '100px 150px'
+            }
+        }}
+        {...rest}
+    >
+        {children}
+    </ReactModal>
 }
 
 export default Modal;
