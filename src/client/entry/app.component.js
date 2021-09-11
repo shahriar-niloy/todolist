2961930bbfd4472b90ab2f0b8b169de69e40339b
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-11 11:38:28 +0600

    Setup scss style structure and make nodemon watch for changes in scss files

--- src/client/entry/app.component.js
+++ src/client/entry/app.component.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Routes from "../components/route/routes.component";
 import { getMyProfileAction } from '../store/actions/user.actions';
-import './app.css';
+import '../style/app.scss';
 
 const App = () => {
     const dispatch = useDispatch();
