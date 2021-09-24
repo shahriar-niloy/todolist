4aca0f47b47a204d0c60938ede0d05166d35f484
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-09-24 18:20:34 +0600

    Change no task component style

--- src/client/components/task/no-task.component.js
+++ src/client/components/task/no-task.component.js
@@ -4,6 +4,7 @@ import LaunchGraphic from '../graphic/launch.graphic';
 export default function NoTask() {
     return <div className="notask">
         <LaunchGraphic className="notask-img" />
-        <h5> No pending tasks <br/> Add a new task to get started </h5>
+        <h5>No pending tasks</h5>
+        <h6>Add a new task to get started</h6>
     </div>
 }
