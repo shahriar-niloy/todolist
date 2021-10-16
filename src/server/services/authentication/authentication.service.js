56e3758e6ac02787b7c961439129d1b727518043
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-10-17 02:16:15 +0600

    Fix no projects after loging in

--- src/server/services/authentication/authentication.service.js
+++ src/server/services/authentication/authentication.service.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const UserModel = require(path.join(process.cwd(), 'src/server/models/user.model'));
+const ProjectModel = require(path.join(process.cwd(), 'src/server/models/project.model'));
 
 async function login(email, password) {
     const user = await UserModel.findOne({ 
-        where: { email: email.toLowerCase() }
+        where: { email: email.toLowerCase() },
+        include: ProjectModel
     });
 
     if (!user || !user.isValidPassword(password)) return;
