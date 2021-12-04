a6dae6f5debe8f0c76c1818740a29a6571d4702c
Al Shahriar Niloy <Shahriar.Niloy@brainstation23.com>
2021-12-04 16:50:34 +0600

    Reuse processTaskList from utilty in task reducer

--- src/client/store/reducers/task.reducer.js
+++ src/client/store/reducers/task.reducer.js
@@ -1,55 +1,9 @@
 import actions from "../../constants/action.types";
+import { processTaskList } from "../../utility";
 
 const initialState = {
     details: null,
     list: {}
-};
-
-function processTaskList(tasks) {
-    const taskToSubtask = new Map();
-    const subtaskToTask = new Map();
-    const tasksByID = {};
-
-    for (const task of tasks) {
-        const parent_id = task.parent_task_id;
-        
-        taskToSubtask.has(parent_id)
-            ? taskToSubtask.get(parent_id).push(task)
-            : taskToSubtask.set(parent_id, [task]);
-
-        subtaskToTask.set(task.id, parent_id);
-        tasksByID[task.id] = task;
-    }
-
-    const parentTasks = tasks.filter(task => !task.parent_task_id);
-
-    parentTasks.forEach((parentTask, index) => {
-        function createSubTaskHierarchy(taskID) {
-            const children = taskToSubtask.get(taskID);
-            
-            if (!children || !children.length) return null;
-
-            let i = 0;
-            for (const child of children) {
-                children[i].subtasks = createSubTaskHierarchy(child.id);
-                ++i;
-            }
-            
-            return children;
-        }
-
-        const subTaskHierarchy = createSubTaskHierarchy(parentTask.id);
-
-        parentTasks[index].subtasks = subTaskHierarchy;
-    });
-
-    return { 
-        flat: tasks,
-        tree: parentTasks,
-        taskToSubtask,
-        subtaskToTask,
-        tasksByID
-    }
 };
 
 export default function taskReducer(state = initialState, action) {
