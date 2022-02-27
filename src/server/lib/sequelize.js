233ad233a0ae945bd56a2d984c17d4554c70a35a
Al Shahriar Niloy <shahriar_niloy@outlook.com>
2022-02-28 03:46:44 +0600

    Turn on Sequelize ssl

--- src/server/lib/sequelize.js
+++ src/server/lib/sequelize.js
@@ -2,6 +2,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const config = require(path.join(process.cwd(), 'src/server/config/config'));
 
-const sequelize = new Sequelize(config.DATABASE_STRING, { logging: false });
+const sequelize = new Sequelize(config.DATABASE_STRING, { 
+    logging: false,
+    dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+        //   rejectUnauthorized: false // This line will fix new error
+        }
+    }
+});
 
 module.exports = sequelize;
