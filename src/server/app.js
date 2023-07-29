6d413e0d99504bb89172235e5f17b9f9cc578bdb
Al Shahriar Niloy <shahriar_niloy@outlook.com>
2023-07-30 02:08:14 +0600

    Add healthcheck

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const axios = require('path');
 
 const eventManager = require(path.join(process.cwd(), 'src/server/lib/events'));
 const eventConstants = require(path.join(process.cwd(), 'src/server/constants/event.constants'));
@@ -8,4 +9,19 @@ function configureEvents() {
 }
 
 configureEvents();
+
+(function() {
+    if (!process.env.HEALTH_CHECK_URL) return;
+
+    setInterval(
+        async function() {
+            try {
+                await axios.get(process.env.HEALTH_CHECK_URL);
+            } catch(err) {
+                console.error(err);
+            }
+        },
+        10 * 60 * 1000
+    );
+})();
 
