34295e5359faf3bc60b85e656d4d0c48536bb17e
Al Shahriar Niloy <shahriar_niloy@outlook.com>
2023-07-30 20:47:58 +0600

    Fix bug

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const axios = require('path');
+const axios = require('axios');
 
 const eventManager = require(path.join(process.cwd(), 'src/server/lib/events'));
 const eventConstants = require(path.join(process.cwd(), 'src/server/constants/event.constants'));
@@ -16,7 +16,8 @@ configureEvents();
     setInterval(
         async function() {
             try {
-                await axios.get(process.env.HEALTH_CHECK_URL);
+                const { data } = await axios.get(process.env.HEALTH_CHECK_URL);
+                console.log(data);
             } catch(err) {
                 console.error(err);
             }
